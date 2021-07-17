11c80d6480b2e753ad136721171e1ee04cbf1589
Erik <Erik@DESKTOP-FFKLVQ5>
2021-07-17 13:13:49 +0500

    Добавьте файлы проекта.

---
+++ Lesson2pod2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2pod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение переменной а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение переменной b: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine("Ошибка! Переменная а > b");
+            int i, sum = 0;
+            for (i = a; b >= i; i++)
+                sum += i;
+            Console.WriteLine("Сумма чисел равна: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
